7a98be88430f754a3e03afa63f4f2d121653b3a8
Vedant Chainani <vedantchainani1084@gmail.com>
2026-01-04 06:45:21 +0530

    chore: add more glob tests

--- packages/core/test/parser/directives.test.ts
+++ packages/core/test/parser/directives.test.ts
@@ -59,4 +59,53 @@ describe("Directive Parsing", () => {
       }
     }),
   );
+
+  it.effect("should return none for line without @ prefix", () =>
+    Effect.gen(function* () {
+      const line = "command key=value";
+      const res = parseDirectiveLine(line);
+      expect(Option.isNone(res)).toBe(true);
+    }),
+  );
+
+  it.effect("should handle empty arg values", () =>
+    Effect.gen(function* () {
+      const line = `@command key=""`;
+      const res = parseDirectiveLine(line);
+      expect(Option.isSome(res)).toBe(true);
+      if (Option.isSome(res)) {
+        expect(res.value.args).toEqual({ key: "" });
+      }
+    }),
+  );
+
+  it.effect("should handle special character in command", () =>
+    Effect.gen(function* () {
+      const line = `@#command key="val"`;
+      const res = parseDirectiveLine(line);
+      expect(Option.isSome(res)).toBe(false);
+    }),
+  );
+
+  it.effect("should handle special character in values", () =>
+    Effect.gen(function* () {
+      const line = `@command key="val#1 $"`;
+      const res = parseDirectiveLine(line);
+      expect(Option.isSome(res)).toBe(true);
+      if (Option.isSome(res)) {
+        expect(res.value.args).toEqual({ key: "val#1 $" });
+      }
+    }),
+  );
+
+  it.effect("should handle malformed key-value pair", () =>
+    Effect.gen(function* () {
+      const line = `@command key='val" key2="val2"`;
+      const res = parseDirectiveLine(line);
+      expect(Option.isSome(res)).toBe(true);
+      if (Option.isSome(res)) {
+        expect(res.value.args).toEqual({ key: `'val"`, key2: "val2" });
+      }
+    }),
+  );
 });
